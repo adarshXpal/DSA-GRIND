e85662522d8eb5fc1039159527b10e22a9b650cb
Adarsh <protoyoru07@gmail.com>
2023-12-01 21:29:46 +0530

    First Commit to this repo

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a=9;
+	cout<<"hello this is testing to test lazygit"<<endl;
+	cout<<"the value of a is "<<a<<endl;
+	
+
+
+return 0;
+}
+
