c7feb2fa9a6ca7e1e14a5ed67281704530b9992d
Adarsh <protoyoru07@gmail.com>
2024-07-17 01:08:41 +0530

    kuch bhi

--- BinarySearch/IterativeBinarySearch.cpp
+++ BinarySearch/IterativeBinarySearch.cpp
@@ -5,7 +5,7 @@ bool BinarySearch(vector<int>&arr,int target){
 	int n=arr.size();
 	int low=0,high=n-1;
 	while(low<=high){
-		int mid=(low+high)/2;
+	int mid=(low+high)/2;
 		if(arr[mid]==target){
 			return true;
 		}else if(arr[mid]<target){
--- BinarySearch/Upper_Bound.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-	cout<<"hello im gioind fd "<<endl;
-return 0;
-}
-
